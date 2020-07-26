af553dc8d5a6cd69c637439bfd7362ce7651d390
Raghu <konidelaraghu@gmail.com>
2020-07-26 13:00:20 +0100

    Posting data with Bodyparser, saving data

--- app.js
+++ app.js
@@ -1,27 +1,39 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
-//connecting to MongoDB
+// connecting to MongoDB
 const db = mongoose.connect('mongodb://localhost/bookAPI');
 const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
 
 const Book = require('./models/bookModel');
 
-bookRouter.route('/books').get((req, res) => {
-  // const response = { hello: 'This is my get API' };
-  const query = {};
-  if (req.query.genre) {
-    query.genre = req.query.genre;
-  }
-  Book.find(query, (err, books) => {
-    if (err) {
-      return res.send(err);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+bookRouter
+  .route('/books')
+  .post((req, res) => {
+    const book = new Book(req.body);
+
+    book.save();
+    return res.status(201).json(book);
+  })
+  .get((req, res) => {
+    // const response = { hello: 'This is my get API' };
+    const query = {};
+    if (req.query.genre) {
+      query.genre = req.query.genre;
     }
-    return res.json(books);
+    Book.find(query, (err, books) => {
+      if (err) {
+        return res.send(err);
+      }
+      return res.json(books);
+    });
   });
-});
 //Get single item
 bookRouter.route('/books/:bookId').get((req, res) => {
   // const response = { hello: 'This is my get API' };
