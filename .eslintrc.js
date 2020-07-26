2cd7eadd363b2f0fb696e4a404333220d69f711c
Raghu <konidelaraghu@gmail.com>
2020-07-26 12:39:51 +0100

    Implementing http get, mongodb, getting item from db

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,12 @@ module.exports = {
     browser: true,
     es2020: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ['airbnb-base'],
   parserOptions: {
     ecmaVersion: 11,
     sourceType: 'module',
   },
   rules: {
+    'comma-dangle': 0,
   },
 };
--- app.js
+++ app.js
@@ -1,7 +1,39 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+//connecting to MongoDB
+const db = mongoose.connect('mongodb://localhost/bookAPI');
+const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
+
+const Book = require('./models/bookModel');
+
+bookRouter.route('/books').get((req, res) => {
+  // const response = { hello: 'This is my get API' };
+  const query = {};
+  if (req.query.genre) {
+    query.genre = req.query.genre;
+  }
+  Book.find(query, (err, books) => {
+    if (err) {
+      return res.send(err);
+    }
+    return res.json(books);
+  });
+});
+//Get single item
+bookRouter.route('/books/:bookId').get((req, res) => {
+  // const response = { hello: 'This is my get API' };
+  Book.findById(req.params.bookId, (err, book) => {
+    if (err) {
+      return res.send(err);
+    }
+    return res.json(book);
+  });
+});
+
+app.use('/api', bookRouter);
 
 app.get('/', (req, res) => {
   res.send('Welcome to Rest & Nodemon API');
